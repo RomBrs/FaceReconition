cbc73f11b87be6817da8b75d8687befd2ccd1d40
RomBrs <38348786+RomBrs@users.noreply.github.com>
2020-08-19 15:02:10 +0300

    add_output

--- src/utils/blur_detection.py
+++ src/utils/blur_detection.py
@@ -1,12 +1,16 @@
 import cv2
 import numpy as np
 
-def is_blurred(image, var_limit):
+def is_blurred(image, var_limit, return_var_limit = False):
     """ Laplace
     """
     laplacian = cv2.Laplacian(image, cv2.CV_64F)
     variance = np.var(laplacian)
     if variance < var_limit:
+        if return_var_limit:
+            return True, variance    
         return True
     else:
+        if return_var_limit:
+            return False, variance
         return False
